d3df8df89feb56343e9f2fd182ab15fde512a253
vishwaka-kamlesh <137268987+vishwaka-kamlesh@users.noreply.github.com>
2023-07-18 11:47:01 +0700

    Create Ceiling.java

---
+++ Ceiling.java
@@ -0,0 +1,35 @@
+class Ceiling {
+    
+    // Binary search method to find the target element in the array
+    static int binarySearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        // Loop until the start and end indices converge
+        while (start <= end) {
+            // Calculate the middle index
+            int mid = start + (end - start) / 2;
+            
+            // Compare the target with the element at the middle index
+            if (nums[mid] == target)
+                return mid; // Target found at the middle index
+            else if (nums[mid] < target)
+                start = mid + 1; // Target is in the upper half
+            else
+                end = mid - 1; // Target is in the lower half
+        }
+        
+        return start; // Target not found, return the start index (ceil)
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = { 40, 50, 60, 70, 90, 100 };
+        int target = 85;
+        
+        int result = binarySearch(nums, target);
+        if (result != -1)
+            System.out.println(result); // Print the index of the found element
+        else
+            System.out.println("Element not found");
+    }
+}
